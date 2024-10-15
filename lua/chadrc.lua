f849b1cb3ef0f505b902a05976938fec5e884ec7
John Züchler <johnzuchler@gmail.com>
2024-10-15 15:30:55 +0200

    update remanining non-lsp keymaps, start exchanging nvim-tree with neotree

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,23 +1,22 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-
 M.nvdash = {
-  load_on_startup = true
+  load_on_startup = true,
 }
 
 M.ui = {
   statusline = {
-    order = { "modalmode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
     modules = {
-      modalmode = function ()
+      modalmode = function()
         -- NVchad's Mode implementation, but with support for libmodal modes
         local config = require("nvconfig").ui.statusline
-        local utils = require("nvchad.stl.utils")
+        local utils = require "nvchad.stl.utils"
 
         local sep_style = config.separator_style
         local sep_icons = utils.separators
@@ -37,8 +36,22 @@ M.ui = {
         local current_mode = "%#St_" .. modes[m][2] .. "Mode#  " .. displaymode
         local mode_sep1 = "%#St_" .. modes[m][2] .. "ModeSep#" .. sep_r
         return current_mode .. mode_sep1 .. "%#ST_EmptySpace#" .. sep_r
-      end
-    }
+      end,
+    },
+  },
+  tabufline = {
+    order = { "neoTreeOffset", "buffers", "tabs", "btns" },
+    modules = {
+      neoTreeOffset = function()
+        local width = 0
+        for _, win in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+          if vim.bo[vim.api.nvim_win_get_buf(win)].ft == "neo-tree" then
+            width = vim.api.nvim_win_get_width(win)
+          end
+        end
+        return "%#NvimTreeNormal#" .. string.rep(" ", width) .. "│"
+      end,
+    },
   },
 }
 
@@ -48,18 +61,17 @@ M.term = {
     col = 0.15,
     height = 0.75,
     width = 0.70,
-  }
+  },
 }
 
 M.base46 = {
-	theme = "nord",
+  theme = "nord",
   transparency = false,
 
-	hl_override = {
+  hl_override = {
     NvDashAscii = { bg = "NONE", fg = "blue" },
     NvDashButtons = { bg = "NONE" },
-	},
-
+  },
 }
 
 return M
