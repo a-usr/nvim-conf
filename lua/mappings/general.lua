780fe55596d8301901f38c2ec1b593affce8c48b
John <81042605+a-usr@users.noreply.github.com>
2024-10-15 18:31:03 +0200

    moar stuf

--- lua/mappings/general.lua
+++ lua/mappings/general.lua
@@ -120,35 +120,37 @@ return require("mappings.util").Map {
   {
     {
       "<leader>e",
-      function()
-        local reveal_file = vim.fn.expand "%:p"
-        if reveal_file == "" then
-          reveal_file = vim.fn.getcwd()
-        else
-          local f = io.open(reveal_file, "r")
-          if f then
-            f.close(f)
-          else
-            reveal_file = vim.fn.getcwd()
-          end
-        end
-        require("neo-tree.command").execute {
-          action = "focus", -- OPTIONAL, this is the default value
-          source = "filesystem", -- OPTIONAL, this is the default value
-          position = "left", -- OPTIONAL, this is the default value
-          reveal_file = reveal_file, -- path to file or folder to reveal
-          reveal_force_cwd = false, -- change cwd without asking if needed
-        }
-      end,
+      "<cmd>NvimTreeFocus<CR>",
+      -- function()
+      --   local reveal_file = vim.fn.expand "%:p"
+      --   if reveal_file == "" then
+      --     reveal_file = vim.fn.getcwd()
+      --   else
+      --     local f = io.open(reveal_file, "r")
+      --     if f then
+      --       f.close(f)
+      --     else
+      --       reveal_file = vim.fn.getcwd()
+      --     end
+      --   end
+      --   require("neo-tree.command").execute {
+      --     action = "focus", -- OPTIONAL, this is the default value
+      --     source = "filesystem", -- OPTIONAL, this is the default value
+      --     position = "left", -- OPTIONAL, this is the default value
+      --     reveal_file = reveal_file, -- path to file or folder to reveal
+      --     reveal_force_cwd = false, -- change cwd without asking if needed
+      --   }
+      -- end,
       desc = "FileTree Focus",
     },
     {
       "<C-n>",
-      function()
-        require("neo-tree.command").execute {
-          toggle = true,
-        }
-      end,
+      -- function()
+      --   require("neo-tree.command").execute {
+      --     toggle = true,
+      --   }
+      -- end,
+      "<cmd>NvimTreeToggle<CR>",
       desc = "FileTree Toggle",
     },
   },
