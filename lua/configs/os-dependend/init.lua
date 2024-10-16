e313b9187b44177a24db577b6490382a60e221b0
John Züchler <johnzuchler@gmail.com>
2024-10-16 23:11:13 +0200

    idk

--- lua/configs/os-dependend/init.lua
+++ lua/configs/os-dependend/init.lua
@@ -1,16 +1,13 @@
-
-
-if vim.fn.has("win32")==1 then
-  return require("configs.os-dependend.windows")
+if vim.fn.has "win32" == 1 then
+  return require "configs.os-dependend.windows"
 else
-  if vim.fn.executable("termux-setup-storage") == 1 then
-    return require("configs.os-dependend.termux")
+  if vim.fn.executable "termux-setup-storage" == 1 then
+    return require "configs.os-dependend.termux"
   else
-    if vim.system({"rg", "-x", "NAME=(.*)"}) == "NAME=NixOS" then
-      return require("configs.os-dependend.nixos")
+    if vim.system({ "grep", "-e", "^NAME=.*", "/etc/os-release" }):wait().stdout == "NAME=NixOS\n" then
+      return require "configs.os-dependend.nixos"
     else
-      return require("configs.os-dependend.linux")
+      return require "configs.os-dependend.linux"
     end
   end
 end
-
