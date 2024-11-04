213e87d7f156c7ff45838faaf7c9341fe9c05d2e
John Züchler <johnzuchler@gmail.com>
2024-11-04 08:47:09 +0100

    enable direnv only on nixos

--- lua/configs/os-dependend/nixos/init.lua
+++ lua/configs/os-dependend/nixos/init.lua
@@ -1,6 +1,7 @@
 local M = require "configs.os-dependend.defaults"
 
 M.plugins.markdown_preview.build = "cd app && yarn install"
+M.plugins.direnv.enable = true
 M.options.shell = "fish"
 
 return M
