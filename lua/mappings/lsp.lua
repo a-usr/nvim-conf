f279ffadcc2a3e2b46e170240624286d352f2ba0
John <81042605+a-usr@users.noreply.github.com>
2024-11-06 06:56:14 +0100

    add edgy.nvim and more lsp mappings

--- lua/mappings/lsp.lua
+++ lua/mappings/lsp.lua
@@ -1,64 +1,106 @@
 local util = require "mappings.util"
 
 return util.Map {
-  "<leader>l",
+
   on = "LSP attach",
-  group = "LSP",
   {
+    "<leader>l",
+    group = "LSP",
     {
-      "t",
-      function()
-        require("lsp_lines").toggle()
-      end,
-      desc = "Toggle Lsp Lines",
-    },
-    {
-      "d",
-      group = "Diagnostics",
+      {
+        "t",
+        function()
+          require("lsp_lines").toggle()
+        end,
+        desc = "Toggle Lsp Lines",
+      },
+      {
+        "c",
+        function()
+          vim.lsp.buf.code_action()
+        end,
+        desc = "Code Actions",
+      },
+      {
+        "d",
+        group = "Diagnostics",
+        {
+          "s",
+          function()
+            require("trouble").toggle { mode = "diagnostics", filter = { buf = 0 } }
+          end,
+          desc = "LSP Show Diagnostics for current buffer",
+        },
+        {
+          "l",
+          function()
+            require("trouble").toggle { mode = "diagnostics" }
+          end,
+          desc = "LSP Show Diagnostics",
+        },
+        {
+          "a",
+          function()
+            require("telescope.builtin").diagnostics()
+          end,
+          desc = "LSP Show Diagnostics in Telescope",
+        },
+      },
       {
         "s",
-        function()
-          require("trouble").toggle { mode = "diagnostics", filter = { buf = 0 } }
-        end,
-        desc = "LSP Show Diagnostics for current buffer",
-      },
-      {
-        "l",
-        function()
-          require("trouble").toggle { mode = "diagnostics" }
-        end,
-        desc = "LSP Show Diagnostics",
-      },
-      {
-        "a",
-        function()
-          require("telescope.builtin").diagnostics()
-        end,
-        desc = "LSP Show Diagnostics in Telescope",
+        group = "Symbol(s)",
+        {
+          "l",
+          function()
+            require("telescope.builtin").lsp_document_symbols()
+          end,
+          desc = "LSP List Document Symbols",
+        },
+        {
+          "r",
+          function()
+            require "nvchad.lsp.renamer"()
+          end,
+          desc = "LSP Rename Symbol",
+        },
+        {
+          "h",
+          vim.lsp.buf.signature_help,
+          desc = "LSP Show Signature Help",
+        },
       },
     },
+  },
+  {
+    "g",
+    group = "Go To",
     {
-      "s",
-      group = "Symbol(s)",
-      {
-        "l",
-        function()
-          require("telescope.builtin").lsp_document_symbols()
-        end,
-        desc = "LSP List Document Symbols",
-      },
-      {
-        "r",
-        function()
-          require "nvchad.lsp.renamer"()
-        end,
-        desc = "LSP Rename Symbol",
-      },
-      {
-        "h",
-        vim.lsp.buf.signature_help,
-        desc = "LSP Show Signature Help",
-      },
+      "d",
+      function()
+        vim.lsp.buf.definition()
+      end,
+      desc = "Go to Definition",
+    },
+    {
+      "D",
+      function()
+        vim.lsp.buf.declaration()
+      end,
+      desc = "Go to Declaration",
+    },
+    {
+      "i",
+      function()
+        vim.lsp.buf.implementation()
+      end,
+      desc = "Go to Implementation",
+    },
+    {
+      "r",
+      function()
+        vim.lsp.buf.references()
+      end,
+      desc = "Go to References",
     },
   },
 }
