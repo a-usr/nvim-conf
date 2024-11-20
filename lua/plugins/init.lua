85382829b64419f167e85e6b15f94c3564b93d9a
John Züchler <johnzuchler@gmail.com>
2024-11-20 08:54:05 +0100

    debug performance issues (Only on windows???) and improve ui a bit with dressing

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,10 +25,6 @@ return {
   -- These are some examples, uncomment them if you want to see them work!
   {
     "2kabhishek/nerdy.nvim",
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
     cmd = "Nerdy",
   },
   {
