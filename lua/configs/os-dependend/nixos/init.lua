696eadb7a1a83400431cac80606836fba3e01635
John <81042605+a-usr@users.noreply.github.com>
2024-12-16 17:11:28 +0100

    move to nushell on nixos

--- lua/configs/os-dependend/nixos/init.lua
+++ lua/configs/os-dependend/nixos/init.lua
@@ -2,6 +2,6 @@ local M = require "configs.os-dependend.defaults"
 
 M.plugins.markdown_preview.build = "cd app && yarn install"
 M.plugins.direnv.enable = true
-M.options.shell = "fish"
+M.options.shell = "nu"
 
 return M
