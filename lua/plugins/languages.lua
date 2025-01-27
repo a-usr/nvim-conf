03bf2e7a364ea1d05ba7b311a362c5ee406938c7
John <81042605+a-usr@users.noreply.github.com>
2025-01-27 21:24:09 +0100

    markview

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -175,4 +175,14 @@ return {
       "MPUploadAll",
     },
   },
+  {
+    "OXY2DEV/markview.nvim",
+    -- lazy = false,      -- Recommended
+    ft = "markdown", -- If you decide to lazy-load anyway
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
